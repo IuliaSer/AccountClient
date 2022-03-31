e66c4d8570504dc9d7c707777bbdff42866fd3ea
iulia_serzantova <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>
2022-03-31 13:43:41 +0300

    "change"

--- src/main/java/com/example/springaccountclient/Main/Main.java
+++ src/main/java/com/example/springaccountclient/Main/Main.java
@@ -1,6 +1,5 @@
 package com.example.springaccountclient.Main;
 
-import com.example.springaccountclient.Model.Account;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,21 +10,10 @@ public class Main {
     public static void main(String[] args) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-//        Account newAccount = new Account(20, 4000);
         RestTemplate restTemplate = new RestTemplate();
-//        HttpEntity<Account> requestBody = new HttpEntity<>(newAccount, headers);
-//        ResponseEntity<Account> result
-//                = restTemplate.postForEntity(URL_TRANSFER, requestBody, Account.class);
         Integer sum = 20000;
         HttpEntity<Integer> requestBody = new HttpEntity<>(sum, headers);
         restTemplate.postForEntity(URL_TRANSFER, requestBody, Integer.class);
-//        System.out.println("Status code:" + result.getStatusCode());
-//
-//        if (result.getStatusCode() == HttpStatus.OK) {
-////            Account e = result.getBody();
-//            System.out.println("(Client Side) Account Created: "+ result.getBody());
-//        }
-
     }
 
 }
